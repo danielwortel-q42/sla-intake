762d6745c654ab233fe18cde0d05a5b477d77527
Daniel Wortel <daniel.wortel@q42.nl>
2026-05-13 16:05:13 +0200

    Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
@@ -20,6 +21,9 @@ export default function RootLayout({
       lang="en"
       className={cn("h-full antialiased font-sans", inter.variable)}
     >
+        {children}
+        <Analytics />
+      
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
